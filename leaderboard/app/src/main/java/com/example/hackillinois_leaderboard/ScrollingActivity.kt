2e8cdf2a15076fa55164522a269183a77d47ab11
adrian-szwejk <aszwe2@illinois.edu>
2023-07-03 18:19:12 -0500

    added 3 buttons to limit number of people on leaderboard

--- leaderboard/app/src/main/java/com/example/hackillinois_leaderboard/ScrollingActivity.kt
+++ leaderboard/app/src/main/java/com/example/hackillinois_leaderboard/ScrollingActivity.kt
@@ -21,9 +21,17 @@ class ScrollingActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
         binding.toolbarLayout.title = "Leaderboard for HackIllinois"
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        binding.fab10.setOnClickListener { view ->
+            Snackbar.make(view, "Calling API to get top 10 in leaderboard", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+        binding.fab25.setOnClickListener { view ->
+            Snackbar.make(view, "Calling API to get top 25 in leaderboard", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        binding.fab50.setOnClickListener { view ->
+            Snackbar.make(view, "Calling API to get top 50 in leaderboard", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
